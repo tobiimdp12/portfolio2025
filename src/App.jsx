abb9739418e73f9bf3b5e545a6b3050d40e97cdf
tobiimdp12 <tobiasselva@gmail.com>
2025-12-10 15:10:56 -0300

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar,Welcome } from "#components";
+import { Navbar, Welcome, Dock } from "#components";
 import React from "react";
 
 const App = () => {
@@ -6,6 +6,7 @@ const App = () => {
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   );
 };
